f7e28730101f9b413f15d60cf813ce1f96c7d86f
test <ozmax.planet@gmail.com>
2019-03-14 03:25:17 +0200

    run parted in script mode

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 timedatectl set-ntp true
 
 # partition the /dev/sda
-parted /dev/sda mklabel gpt
+parted -s /dev/sda mklabel gpt
 # make uefi system partion
-parted /dev/sda mkpart primary fat32 1MiB 551MiB
-parted /dev/sda set 1 esp on
+parted -s /dev/sda mkpart primary fat32 1MiB 551MiB
+parted -s /dev/sda set 1 esp on
 # make swap partition
-parted /dev/sda mkpart primary linux-swap 551MiB 1551MiB
+parted -s /dev/sda mkpart primary linux-swap 551MiB 1551MiB
 # make / partition
-parted /dev/sda mkpart primary ext4 1551MiB 100%
+parted -s /dev/sda mkpart primary ext4 1551MiB 100%
 
 # format the partitions
 # format the esp
