284b165100375cc05a1186b5800de5b82176c6f8
test <ozmax.planet@gmail.com>
2019-03-26 23:18:14 +0200

    sdb

--- install.sh
+++ install.sh
@@ -3,30 +3,30 @@
 # set ntp
 timedatectl set-ntp true
 
-# partition the /dev/sda
-parted -s /dev/sda mklabel gpt
+# partition the /dev/sdb
+parted -s /dev/sdb mklabel gpt
 # make uefi system partion
-parted -s /dev/sda mkpart primary fat32 1MiB 551MiB
-parted -s /dev/sda set 1 esp on
+parted -s /dev/sdb mkpart primary fat32 1MiB 551MiB
+parted -s /dev/sdb set 1 esp on
 # make swap partition
-parted -s /dev/sda mkpart primary linux-swap 551MiB 1551MiB
+parted -s /dev/sdb mkpart primary linux-swap 551MiB 1551MiB
 # make / partition
-parted -s /dev/sda mkpart primary ext4 1551MiB 100%
+parted -s /dev/sdb mkpart primary ext4 1551MiB 100%
 
 # format the partitions
 # format the esp
-mkfs.fat -F32 /dev/sda1
+mkfs.fat -F32 /dev/sdb1
 # format the swap partition
-mkswap /dev/sda2
-swapon /dev/sda2
+mkswap /dev/sdb2
+swapon /dev/sdb2
 # format the / partition
-mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sdb3
 
 # mount partitions
-mount /dev/sda3 /mnt
+mount /dev/sdb3 /mnt
 
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/sdb1 /mnt/boot
 
 # set up greek mirrorlist
 curl 'https://www.archlinux.org/mirrorlist/?country=GR&protocol=https' \
@@ -60,7 +60,7 @@ printf "\
 title Arch Linux\n\
 linux /vmlinuz-linux\n\
 initrd /initramfs-linux.img\n\
-options root=PARTUUID="$(blkid -o value -s PARTUUID /dev/sda3)" rw\n\
+options root=PARTUUID="$(blkid -o value -s PARTUUID /dev/sdb3)" rw\n\
 " > /boot/loader/entries/arch.conf
 
 # setup network
